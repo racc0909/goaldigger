b568cfd1fc022cd348a64759d64538a3249f92c3
Linh Ha <racc0909@gmail.com>
2024-06-30 15:51:55 +0200

    I dont know

--- pages/1_Form.py
+++ pages/1_Form.py
@@ -2,13 +2,17 @@ import streamlit as st
 from db import session, FormData
 
 def form_page():
-    st.title("Form Page")
-    data = st.text_input("Enter some data")
-    if st.button("Submit"):
-        new_data = FormData(user_id=st.session_state.user_id, data=data)
-        session.add(new_data)
-        session.commit()
-        st.success("Data submitted")
+    if 'logged_in' in st.session_state and st.session_state.logged_in:
+        st.title("Form Page")
+        data = st.text_input("Enter some data")
+        if st.button("Submit"):
+            new_data = FormData(user_id=st.session_state.user_id, data=data)
+            session.add(new_data)
+            session.commit()
+            st.success("Data submitted")
+    else:
+        st.warning("Please log in to access this page.")
+        st.stop()
 
 if __name__ == "__main__":
     form_page()
