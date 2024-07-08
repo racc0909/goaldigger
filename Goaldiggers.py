6496ac516ab1bf48d36e295c3e4d7f08cdca29f2
Shiya0928 <lsy1073117540@163.com>
2024-07-08 18:19:44 +0200

    Update Goaldiggers.py

--- Goaldiggers.py
+++ Goaldiggers.py
@@ -7,7 +7,8 @@ import time
 # Way one
 # st.logo("data/Goal_Digger-removebg-preview (1)", link="data/Goal_Digger-removebg-preview (1).png", icon_image=None)
 # 定义图片路径
-LOGO_PATH = "data/Goal_Digger-removebg-preview (1).png"
+sidebar_logo = "data/Goal_Digger-removebg-preview (1).png"
+st.logo(sidebar_logo)
 
 # 在侧边栏显示徽标图片，没有链接
 st.sidebar.logo(LOGO_PATH, icon_image=None)
