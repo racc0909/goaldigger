c00878bb62246da3ff725e4232ba2dbbd7b5d534
racc0909 <lelinhha255@gmail.com>
2024-07-13 17:28:05 +0200

    change text color

--- pages/3_Edit_Plan.py
+++ pages/3_Edit_Plan.py
@@ -145,7 +145,7 @@ def editing_page():
                         "company":" ",
                         "description": "We have been assisting Expats for more than 20 years to secure their German mortgage. An easy English speaking step-by-step service which is free of charge. Blue-Card holders welcome. Five star Google Reviews from our clients prove our services.",
                         "link_text": "View more",
-                        "link": "https://www.yourgermanmortgage.com",
+                        "link": "https://your-german-mortgage.de/",
                         "image_path": "img/image_removebg_preview.png"
                     },
                     {
@@ -173,6 +173,7 @@ def editing_page():
                 colors = ["#ffffff", "#0c2c4c", "#24243c", "#ffffff", "#fff3e0"] 
                 border_colors = ["#000000", "#ffffff", "#d81b60", "#8e24aa", "#ffb300"]  # Border colors
                 text_colors = ["#333333", "#ffffff", "#ffffff", "##547e8c", "#333333"]
+                button_text_color = ["#24243c", "#ffffff", "#24243c", "#ffffff", "#24243c"] 
                 
                 col1, col2 = st.columns(2)
                 for i, ad in enumerate(ads):
@@ -188,7 +189,7 @@ def editing_page():
                             <img src="data:image/png;base64,{encoded_image}" width="100%" style="margin: 10px 0;">
                             <p style="color: {text_color};">{ad['description']}</p>
                             <a href="{ad['link']}" target="_blank" style="color: {text_color}; text-decoration: none;">
-                                <button style="background-color: {text_color}; color: red; border: none; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 10px 2px; cursor: pointer; border-radius: 5px;">Click to Apply</button>
+                                <button style="background-color: {text_color}; color: {button_text_color}; border: none; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 10px 2px; cursor: pointer; border-radius: 5px;">Click to Apply</button>
                             </a>
                         </div>
                         """, unsafe_allow_html=True)
