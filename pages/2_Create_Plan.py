10a567175cfa14b93f778dda7f937d607a44f026
Shiya0928 <lsy1073117540@163.com>
2024-07-13 10:45:50 +0200

    Update 2_Create_Plan.py

--- pages/2_Create_Plan.py
+++ pages/2_Create_Plan.py
@@ -21,7 +21,7 @@ def planning_page():
 
         # SHOW PLAN OPTIONS
         st.sidebar.title("Choose a plan")
-        page = st.sidebar.radio("Go to", ["House Buyer Savings Plan 🏡", "Car Buyer Savings Plan", "Retirement Savings Plan", "Customized Financial Plan"])
+        page = st.sidebar.radio("Go to", ["🏡 House Buyer Savings Plan", "Car Buyer Savings Plan", "Retirement Savings Plan", "Customized Financial Plan"])
         
         # SHOW PERSONAL INFORMATION
         st.sidebar.header(f'Your Personal Information')
