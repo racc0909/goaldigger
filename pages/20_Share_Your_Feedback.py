31c8647a02e3ee11cedd7040a4f74d69281a9136
Shiya0928 <lsy1073117540@163.com>
2024-07-14 17:49:13 +0200

    Update 20_Share_Your_Feedback.py

--- pages/20_Share_Your_Feedback.py
+++ pages/20_Share_Your_Feedback.py
@@ -1,4 +1,10 @@
 import streamlit as st
+from db import showChosenPages, logout
+
+showChosenPages()
+
+logout()
+
 def load_css(file_path):
     with open(file_path) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
