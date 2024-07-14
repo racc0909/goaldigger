b77703116e586cd49e2b32fc51b9e11912b15231
Linh Ha <racc0909@gmail.com>
2024-07-14 15:08:05 +0200

    try to fix 2050

--- Goaldigger.py
+++ Goaldigger.py
@@ -207,126 +207,131 @@ def main():
          # Display Timeline
          display_timeline(user_id)
 
-         # Graph for total savings over time
-         savings_data = {'Date': [], 'Total Savings': []}
-         start_date = datetime.today()
-         end_date = max(plan.goal_date if isinstance(plan.goal_date, datetime) else datetime.combine(plan.goal_date, datetime.min.time()) for plan in filtered_plans)
-         current_date = start_date
+         # Check if filtered_plans is not empty
+         if filtered_plans:
+            # Graph for total savings over time
+            savings_data = {'Date': [], 'Total Savings': []}
+            start_date = datetime.today()
+            end_date = max(plan.goal_date if isinstance(plan.goal_date, datetime) else datetime.combine(plan.goal_date, datetime.min.time()) for plan in filtered_plans)
+            current_date = start_date
 
-         while current_date <= end_date:
+            while current_date <= end_date:
                current_monthly_savings = sum(plan.goal_target_monthly for plan in filter_plans_by_date(plans, current_date))
                savings_data['Date'].append(current_date)
                savings_data['Total Savings'].append(current_monthly_savings)
                current_date += timedelta(days=30)  # Move to next month
 
-         savings_df = pd.DataFrame(savings_data)
+            savings_df = pd.DataFrame(savings_data)
 
-         # Create the figure
-         fig_savings = go.Figure()
-         fig_savings.add_trace(go.Scatter(
-            x=savings_df['Date'], 
-            y=savings_df['Total Savings'], 
-            mode='lines+markers', 
-            name='Total Savings',
-            hovertemplate="Year: %{x|%Y}, Total savings: %{y:.2f}<extra></extra>"
-         ))
+            # Create the figure
+            fig_savings = go.Figure()
+            fig_savings.add_trace(go.Scatter(
+               x=savings_df['Date'], 
+               y=savings_df['Total Savings'], 
+               mode='lines+markers', 
+               name='Total Savings',
+               hovertemplate="Year: %{x|%Y}, Total savings: %{y:.2f}<extra></extra>"
+            ))
 
-         # Update layout
-         fig_savings.update_layout(
-            title='Total Savings Over Time',
-            xaxis_title='Date',
-            yaxis_title=f'Total Savings ({profile.user_currency})',
-            xaxis=dict(tickmode='linear', dtick="M12", tickformat="%Y"),
-            showlegend=True,
-            height=300
-         )
-         st.plotly_chart(fig_savings)
+            # Update layout
+            fig_savings.update_layout(
+               title='Total Savings Over Time',
+               xaxis_title='Date',
+               yaxis_title=f'Total Savings ({profile.user_currency})',
+               xaxis=dict(tickmode='linear', dtick="M12", tickformat="%Y"),
+               showlegend=True,
+               height=300
+            )
+            st.plotly_chart(fig_savings)
 
-         # Show each plan
-         # Plans with loans
-         st.subheader(" Saving Plans Summary")
-         col1, col2 = st.columns(2)
-         for i, plan in enumerate([plan for plan in plans if plan.goal_target_monthly]): 
-            total_saving = getTotalSavings(user_id, plan.plan_id)
-            rest_saving = plan.goal_target - total_saving     
-            with col1 if i % 2 == 0 else col2:
-               st.markdown(
-                  f"""
-                  <div id="wholetext" style="background-color:#f4f4f4; padding: 10px; margin: 10px; border-radius: 10px;">
-                        <div class="plan-box">
-                           <h3>{plan.goal_name}</h3>
-                           <p><strong>Target Amount:</strong> {plan.goal_target:,.2f} {profile.user_currency}</p>
-                           <p><strong>Due Date:</strong> {plan.goal_date.strftime('%d.%m.%Y')}</p>
-                           <p style="margin: 1;"><strong>Monthly Savings Needed:</strong> {plan.goal_target_monthly:,.2f} {profile.user_currency}</p>
-                           <p style="margin: 1; color: red;"><strong>Current Savings:</strong> {total_saving:,.2f} {profile.user_currency}</p>
-                           <p style="margin: 1; color: red;"><strong>Rest Amount Needed:</strong> {rest_saving:,.2f} {profile.user_currency}</p>
-                           <div style='background-color:#e0f7fa; padding: 10px; border-radius: 10px;'>
-                              <p style='color: blue;'><strong>Monthly Savings needed for Loan Payment:</strong> {plan.goal_target_monthly:,.2f} {profile.user_currency}</p>
-                              <p><strong>Total Loan Payment:</strong> {plan.loan_amount:,.2f} {profile.user_currency}</p>
-                              <p><strong>Loan End Date:</strong> {plan.loan_startdate.strftime('%d.%m.%Y')} to {(plan.loan_startdate + pd.DateOffset(years=plan.loan_duration)).strftime('%d.%m.%Y')}</p>
+            # Show each plan
+            # Plans with loans
+            st.subheader(" Saving Plans Summary")
+            col1, col2 = st.columns(2)
+            for i, plan in enumerate([plan for plan in filtered_plans if plan.goal_target_monthly]): 
+               total_saving = getTotalSavings(user_id, plan.plan_id)
+               rest_saving = plan.goal_target - total_saving     
+               with col1 if i % 2 == 0 else col2:
+                  st.markdown(
+                     f"""
+                     <div id="wholetext" style="background-color:#f4f4f4; padding: 10px; margin: 10px; border-radius: 10px;">
+                           <div class="plan-box">
+                              <h3>{plan.goal_name}</h3>
+                              <p><strong>Target Amount:</strong> {plan.goal_target:,.2f} {profile.user_currency}</p>
+                              <p><strong>Due Date:</strong> {plan.goal_date.strftime('%d.%m.%Y')}</p>
+                              <p style="margin: 1;"><strong>Monthly Savings Needed:</strong> {plan.goal_target_monthly:,.2f} {profile.user_currency}</p>
+                              <p style="margin: 1; color: red;"><strong>Current Savings:</strong> {total_saving:,.2f} {profile.user_currency}</p>
+                              <p style="margin: 1; color: red;"><strong>Rest Amount Needed:</strong> {rest_saving:,.2f} {profile.user_currency}</p>
+                              <div style='background-color:#e0f7fa; padding: 10px; border-radius: 10px;'>
+                                 <p style='color: blue;'><strong>Monthly Savings needed for Loan Payment:</strong> {plan.goal_target_monthly:,.2f} {profile.user_currency}</p>
+                                 <p><strong>Total Loan Payment:</strong> {plan.loan_amount:,.2f} {profile.user_currency}</p>
+                                 <p><strong>Loan End Date:</strong> {plan.loan_startdate.strftime('%d.%m.%Y')} to {(plan.loan_startdate + pd.DateOffset(years=plan.loan_duration)).strftime('%d.%m.%Y')}</p>
+                              </div>
                            </div>
-                        </div>
-                  </div>
-                  """, unsafe_allow_html=True
-               )
-            
-               col1_1, col1_2, col1_3, col1_4 = st.columns([1, 1, 0.8, 1])
-               with col1_1:
-                  if st.button(f"✅ Add Saving", key=f"add_saving_{plan.plan_id}_{i}"):
-                     st.session_state.add_saving_plan_id = plan.plan_id
-                     st.switch_page("pages/8_Add_Saving.py")
-               with col1_2:
-                  if st.button(f"📈 Invest", key=f"invest_{plan.plan_id}_{i}"):
-                     st.session_state.invest_plan_id = plan.plan_id
-                     st.switch_page("pages/7_Risk_Tolerance_Assessment.py")
-               with col1_3:
-                  if st.button(f"✏️ View", key=f"edit_{plan.plan_id}_{i}"):
-                     st.session_state.edit_plan_id = plan.plan_id
-                     st.switch_page("pages/3_Edit_Plan.py")
-               with col1_4:
-                  if st.button(f"🗑️ Delete", key=f"delete_{plan.plan_id}_{i}"):
-                     deletePlan(plan.plan_id)
-                     st.experimental_rerun()
+                     </div>
+                     """, unsafe_allow_html=True
+                  )
+               
+                  col1_1, col1_2, col1_3, col1_4 = st.columns([1, 1, 0.8, 1])
+                  with col1_1:
+                     if st.button(f"✅ Add Saving", key=f"add_saving_{plan.plan_id}_{i}"):
+                        st.session_state.add_saving_plan_id = plan.plan_id
+                        st.switch_page("pages/8_Add_Saving.py")
+                  with col1_2:
+                     if st.button(f"📈 Invest", key=f"invest_{plan.plan_id}_{i}"):
+                        st.session_state.invest_plan_id = plan.plan_id
+                        st.switch_page("pages/7_Risk_Tolerance_Assessment.py")
+                  with col1_3:
+                     if st.button(f"✏️ View", key=f"edit_{plan.plan_id}_{i}"):
+                        st.session_state.edit_plan_id = plan.plan_id
+                        st.switch_page("pages/3_Edit_Plan.py")
+                  with col1_4:
+                     if st.button(f"🗑️ Delete", key=f"delete_{plan.plan_id}_{i}"):
+                        deletePlan(plan.plan_id)
+                        st.experimental_rerun()
 
-         # Plans without loans
-         st.subheader("")
-         col1, col2 = st.columns(2)
-         for i, plan in enumerate([plan for plan in plans if plan.goal_target_monthly == False]):
-            total_saving = getTotalSavings(user_id, plan.plan_id)
-            rest_saving = plan.goal_target - total_saving     
-            with col1 if i % 2 == 0 else col2:
-               st.markdown(
-                  f"""
-                  <div id="wholetext" style="background-color:#f4f4f4; padding: 10px; margin: 10px; border-radius: 10px;">
-                        <div class="plan-box">
-                           <h3>{plan.goal_name}</h3>
-                           <p><strong>Target Amount:</strong> {plan.goal_target:,.2f} {profile.user_currency}</p>
-                           <p><strong>Due Date:</strong> {plan.goal_date.strftime('%d.%m.%Y')}</p>
-                           <p style="margin: 1;"><strong>Monthly Savings Needed:</strong> {plan.goal_target_monthly:,.2f} {profile.user_currency}</p>
-                           <p style="margin: 1; color: red;"><strong>Current Savings:</strong> {total_saving:,.2f} {profile.user_currency}</p>
-                           <p style="margin: 1; color: red;"><strong>Rest Amount Needed:</strong> {rest_saving:,.2f} {profile.user_currency}</p>
-                        </div>
-                  </div>
-                  """, unsafe_allow_html=True
-               )
-            
-               col1_1, col1_2, col1_3, col1_4 = st.columns([1, 1, 0.8, 1])
-               with col1_1:
-                  if st.button(f"✅ Add Saving", key=f"add_saving_{plan.plan_id}_{i}"):
-                     st.session_state.add_saving_plan_id = plan.plan_id
-                     st.switch_page("pages/8_Add_Saving.py")
-               with col1_2:
-                  if st.button(f"📈 Invest", key=f"invest_{plan.plan_id}_{i}"):
-                     st.session_state.invest_plan_id = plan.plan_id
-                     st.switch_page("pages/7_Risk_Tolerance_Assessment.py")
-               with col1_3:
-                  if st.button(f"✏️ Edit", key=f"edit_{plan.plan_id}_{i}"):
-                     st.session_state.edit_plan_id = plan.plan_id
-                     st.switch_page("pages/3_Edit_Plan.py")
-               with col1_4:
-                  if st.button(f"🗑️ Delete", key=f"delete_{plan.plan_id}_{i}"):
-                     deletePlan(plan.plan_id)
-                     st.experimental_rerun()
+            # Plans without loans
+            st.subheader("")
+            col1, col2 = st.columns(2)
+            for i, plan in enumerate([plan for plan in filtered_plans if plan.goal_target_monthly == False]):
+               total_saving = getTotalSavings(user_id, plan.plan_id)
+               rest_saving = plan.goal_target - total_saving     
+               with col1 if i % 2 == 0 else col2:
+                  st.markdown(
+                     f"""
+                     <div id="wholetext" style="background-color:#f4f4f4; padding: 10px; margin: 10px; border-radius: 10px;">
+                           <div class="plan-box">
+                              <h3>{plan.goal_name}</h3>
+                              <p><strong>Target Amount:</strong> {plan.goal_target:,.2f} {profile.user_currency}</p>
+                              <p><strong>Due Date:</strong> {plan.goal_date.strftime('%d.%m.%Y')}</p>
+                              <p style="margin: 1;"><strong>Monthly Savings Needed:</strong> {plan.goal_target_monthly:,.2f} {profile.user_currency}</p>
+                              <p style="margin: 1; color: red;"><strong>Current Savings:</strong> {total_saving:,.2f} {profile.user_currency}</p>
+                              <p style="margin: 1; color: red;"><strong>Rest Amount Needed:</strong> {rest_saving:,.2f} {profile.user_currency}</p>
+                           </div>
+                     </div>
+                     """, unsafe_allow_html=True
+                  )
+               
+                  col1_1, col1_2, col1_3, col1_4 = st.columns([1, 1, 0.8, 1])
+                  with col1_1:
+                     if st.button(f"✅ Add Saving", key=f"add_saving_{plan.plan_id}_{i}"):
+                        st.session_state.add_saving_plan_id = plan.plan_id
+                        st.switch_page("pages/8_Add_Saving.py")
+                  with col1_2:
+                     if st.button(f"📈 Invest", key=f"invest_{plan.plan_id}_{i}"):
+                        st.session_state.invest_plan_id = plan.plan_id
+                        st.switch_page("pages/7_Risk_Tolerance_Assessment.py")
+                  with col1_3:
+                     if st.button(f"✏️ Edit", key=f"edit_{plan.plan_id}_{i}"):
+                        st.session_state.edit_plan_id = plan.plan_id
+                        st.switch_page("pages/3_Edit_Plan.py")
+                  with col1_4:
+                     if st.button(f"🗑️ Delete", key=f"delete_{plan.plan_id}_{i}"):
+                        deletePlan(plan.plan_id)
+                        st.experimental_rerun()
+         else:
+            st.info("No plans available to display the savings graph.")
+
 
     else:
          login_page()
